0fd5851823ad80ee1fdcd19479c63e0ae5642df6
Sergei Cherepanov <sc@connect.club>
2018-08-21 19:47:38 +0300

    Fixes

--- formula/ci.rb
+++ formula/ci.rb
@@ -4,6 +4,8 @@ class Ci < Formula
   url "https://github.com/insanehunter/ci/archive/v0.0.2.tar.gz"
   sha256 "894205b4ab171f933ffbcd31a1513074413347d0deba8528fb6afe922bdd050b"
 
+  depends_on "jq"
+
   def install
     bin.install "ci"
   end
