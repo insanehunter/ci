e67f91c638e849f92ef0ea2bf3d8061c25da3cb3
Sergei Cherepanov <sc@connect.club>
2018-08-22 17:27:54 +0300

    Update formula

--- formula/ci.rb
+++ formula/ci.rb
@@ -1,8 +1,8 @@
 class Ci < Formula
   desc "Console utility for building with Jenkins"
   homepage "https://github.com/insanehunter/ci"
-  url "https://github.com/insanehunter/ci/archive/v0.0.3.tar.gz"
-  sha256 "9909f3772cb80440322e4b91bb2a0ae7ceb9e14eecbe2a14cb1106e4f63c834f"
+  url "https://github.com/insanehunter/ci/archive/v0.0.4.tar.gz"
+  sha256 "50f9f88b89bd965a0e3edb853eea36e2131ffc4d8a73f00d18b7d205526bf2bd"
 
   depends_on "jq"
   depends_on "pup"
