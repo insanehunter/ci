93d166491e64fcd2e801406513b36f217f6c25e0
Sergei <s.cherepanov@gmail.com>
2018-10-02 00:17:43 +0300

    Fix shecksum

--- formula/ci.rb
+++ formula/ci.rb
@@ -2,7 +2,7 @@ class Ci < Formula
   desc "Console utility for building with Jenkins"
   homepage "https://github.com/insanehunter/ci"
   url "https://github.com/insanehunter/ci/archive/0.0.5.tar.gz"
-  sha256 "0b66997f0d968e490b9e972fe4bceb592c441b7659ca2519fc6c5a49789afdd7"
+  sha256 "6f05cd049a130dae120c9436aa48b5e5a82f1aa736d5e564e4c933b2dfb9ee36"
 
   depends_on "jq"
   depends_on "pup"
